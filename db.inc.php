bea038b1411d76b4a17d581b322b8e565260d5b1
manoranjanmajumdar <143195446+manoranjanmajumdar@users.noreply.github.com>
2023-11-09 10:10:36 +0530

    Update db.inc.php

--- db.inc.php
+++ db.inc.php
@@ -1,1 +1,7 @@
+<?php
 
+define('DB_SERVER', 'php-database.cbkpu6bsl4ei.us-east-2.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'saranyaSREEDHARAN');
+define('DB_DATABASE', 'php');
+?>
